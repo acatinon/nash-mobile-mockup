21e576de3dae4181a3404f1a6b76a16c9c0c5578
Aurel <aurelien.catinon@gmail.com>
2021-03-11 22:01:40 +0100

    Adding dark mode switch on drawer

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'react-native-elements';
-import { DrawerContentScrollView, DrawerItemList, DrawerItem, DrawerToggleButton, createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import AppLoading from 'expo-app-loading';
 import Wallet from "./src/views/wallet";
+import DrawerContent from "./src/views/drawer_content";
+import { ThemeReducer, ThemeReducerContext, initialState } from './src/helpers/theme_reducer';
 import { padding } from './src/styles/theme';
 
 const theme = {
@@ -37,24 +38,20 @@ const theme = {
 
 const Drawer = createDrawerNavigator();
 
-function DrawerContent(props) {
-  return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
-    </DrawerContentScrollView>
-  );
-}
-
 export default function App() {
+  const [ThemeState, dispatch] = useReducer(ThemeReducer, initialState);
+
   return (
     <SafeAreaProvider>
-      <ThemeProvider theme={theme} useDark={false}>
-        <NavigationContainer>
-          <Drawer.Navigator initialRouteName="Wallet" drawerContent={DrawerContent}>
-            <Drawer.Screen name="Wallet" component={Wallet} />
-          </Drawer.Navigator>
-        </NavigationContainer>
-      </ThemeProvider>
+      <ThemeReducerContext.Provider value={{ ThemeState, dispatch }}>
+        <ThemeProvider theme={theme} useDark={ThemeState.themeMode === 'dark' ? true : false}>
+          <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Wallet" drawerContent={DrawerContent}>
+              <Drawer.Screen name="Wallet" component={Wallet} />
+            </Drawer.Navigator>
+          </NavigationContainer>
+        </ThemeProvider>
+      </ThemeReducerContext.Provider>
     </SafeAreaProvider>
   );
 }
---
+++ src/views/drawer_content.js
@@ -0,0 +1,57 @@
+import React, { useContext } from 'react';
+import { View, Switch } from 'react-native';
+import { ThemeContext, Text, Divider } from 'react-native-elements';
+import { DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+import { ThemeReducerContext } from '../helpers/theme_reducer';
+
+function ContentComponent(props) {
+  const { ThemeState, dispatch } = useContext(ThemeReducerContext);
+  const { theme } = useContext(ThemeContext);
+  
+  return (
+    <DrawerContentScrollView {...props}>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          marginHorizontal: 10,
+          marginVertical: 4,
+        }}
+      >
+        <Text
+          style={{
+            marginTop: 3,
+            flexGrow: 1,
+          }}
+        >
+          Dark Mode
+        </Text>
+
+        <Switch
+          style={{
+          }}
+          value={ThemeState.themeMode === 'dark'}
+          onValueChange={(val) => {
+            if (val === true) {
+              dispatch({ type: 'set-theme', payload: 'dark' });
+            } else {
+              dispatch({ type: 'set-theme', payload: 'light' });
+            }
+          }}
+        />
+      </View>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
+
+function DrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <ContentComponent {...props} />
+    </DrawerContentScrollView>
+  );
+}
+
+
+export default DrawerContent;
